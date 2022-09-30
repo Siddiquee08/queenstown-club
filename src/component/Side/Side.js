9ae2c32f9ab79b1fad5ffb1aed9a01b78e97eb8d
T.M Siddiquee <siddiquee09@gmail.com>
2022-10-01 02:31:39 +0600

    tweaking

--- src/component/Side/Side.js
+++ src/component/Side/Side.js
@@ -11,7 +11,7 @@ const Side = () => {
                  <div>
                  <img src="https://images.unsplash.com/photo-1520975916090-3105956dac38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
            </div>
-            <div>
+            <div className='profile-tag'>
                 <h2 className='heading'>Tashrif Siddiquee</h2>
                 <p>23/A, Dilu Road, Dhaka</p>
             </div>
